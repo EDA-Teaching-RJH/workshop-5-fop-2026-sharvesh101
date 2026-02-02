d9778b52d4f7751015f5ce8722011827c46e0fbe
Sharvesh <sl101@kent.ac.uk>
2026-02-02 14:01:28 +0000

    completed camel.py

--- camel.py
+++ camel.py
@@ -1,1 +1,8 @@
-# Enter your code here
+enter_variable_name = input("Enter variable name: ")
+
+for letter in enter_variable_name:
+    if letter.isupper():
+        print("_"+letter.lower(),end="")
+    else:
+        print(letter,end="")
+print()
